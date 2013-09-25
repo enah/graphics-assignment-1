97d87a197bc997b0cd6c1e7dce55f5325cb80550
enah <ehariyoshi@berkeley.edu>
2013-09-25 09:44:56 -0700

    Saving files: currently does not work

--- example_01.cpp
+++ example_01.cpp
@@ -404,12 +404,96 @@ void myDisplay() {
 
 }
 
+/*
+   windowDump code taken from http://paulbourke.net/miscellaneous/windowdump/
+   Write the current view to a file
+   The multiple fputc()s can be replaced with
+      fwrite(image,width*height*3,1,fptr);
+   If the memory pixel order is the same as the destination file format.
+*/
+int windowDump(void)
+{
+   int i,j;
+   FILE *fptr;
+   //static int counter = 0; /* This supports animation sequences */
+   char fname[32] = "picture.raw";
+   unsigned char *image;
+
+   /* Allocate our buffer for the image */
+   // if ((image = malloc(3*viewport.w*viewport.h*sizeof(char))) == NULL) {
+   //    fprintf(stderr,"Failed to allocate memory for image\n");
+   //    return(0);
+   // }
+
+   glPixelStorei(GL_PACK_ALIGNMENT,1);
+
+   /* Open the file */
+/*
+   if (stereo)
+      sprintf(fname,"L_%04d.raw",counter);
+   else
+*/
+   // sprintf(fname,"C_%04d.raw",counter);
+   if ((fptr = fopen(fname,"w")) == NULL) {
+      fprintf(stderr,"Failed to open file for window dump\n");
+      return(0);
+   }
+
+   /* Copy the image into our buffer */
+   glReadBuffer(GL_BACK_LEFT);
+   glReadPixels(0,0,viewport.w,viewport.h,GL_RGB,GL_UNSIGNED_BYTE,image);
+
+   /* Write the raw file */
+   /* fprintf(fptr,"P6\n%d %d\n255\n",width,height); for ppm */
+   for (j=viewport.h-1;j>=0;j--) {
+      for (i=0;i<viewport.w;i++) {
+         fputc(image[3*j*viewport.w+3*i+0],fptr);
+         fputc(image[3*j*viewport.w+3*i+1],fptr);
+         fputc(image[3*j*viewport.w+3*i+2],fptr);
+      }
+   }
+   fclose(fptr);
+
+   // if (stereo) {
+   //    /* Open the file */
+   //    sprintf(fname,"R_%04d.raw",counter);
+   //    if ((fptr = fopen(fname,"w")) == NULL) {
+   //       fprintf(stderr,"Failed to open file for window dump\n");
+   //       return(FALSE);
+   //    }
+
+   //    /* Copy the image into our buffer */
+   //    glReadBuffer(GL_BACK_RIGHT);
+   //    glReadPixels(0,0,width,height,GL_RGB,GL_UNSIGNED_BYTE,image);
+
+   //    /* Write the raw file */
+   //    /* fprintf(fptr,"P6\n%d %d\n255\n",width,height); for ppm */
+   //    for (j=height-1;j>=0;j--) {
+   //       for (i=0;i<width;i++) {
+   //          fputc(image[3*j*width+3*i+0],fptr);
+   //          fputc(image[3*j*width+3*i+1],fptr);
+   //          fputc(image[3*j*width+3*i+2],fptr);
+   //       }
+   //    }
+   //    fclose(fptr);
+   // }
+
+   /* Clean up */
+   counter++;
+   free(image);
+   return(1);
+}
+
+
+
 void myKeyboard(unsigned char key, int mouseX, int mouseY) {
     switch (key) {
         case ' ':
             exit(0);
             break;
-            
+    	case 's':
+	    windowDump();
+	    break;
         default:
             break;
     }
@@ -437,7 +521,7 @@ int main(int argc, char *argv[]) {
     
     shape = SPHERE;
     multiple = false;
-    mul_num = 200;
+    mul_num = 0;
     toon = false;
     
     int n = 1;
